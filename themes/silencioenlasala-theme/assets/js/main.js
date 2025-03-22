2d3e3e203ce158e41417a21a159df81f199ccfb7
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2025-03-22 19:18:08 +0100

    fix(header): 🐛 fix logo animation

--- themes/silencioenlasala-theme/assets/js/main.js
+++ themes/silencioenlasala-theme/assets/js/main.js
@@ -1,45 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const logo = document.querySelector('.logo'); // Select the image inside .logo
-  const newLogoSrc = '/images/line-moon.png'; // Change this to your new logo image
-  const originalLogoSrc = logo.src;
-  const targetTranslation = -380; // The translation distance on the X axis (left to right)
+  const main = document.querySelector('main'); // Scroll container
+  const body = document.body;
 
-  function handleScroll(e) {
-    if (window.innerWidth < 900) {
-      const scrollPosition = e.currentTarget.scrollTop;
-
-      if (scrollPosition > 100) {
-        const showNew = () => {
-          if (scrollPosition + 200 < Math.abs(targetTranslation)) {
-            logo.src = newLogoSrc;
-            logo.classList.add('logo--scrolled');
-
-            logo.style.transform = `translateX(${
-              targetTranslation + scrollPosition + 200
-            }px)`;
-          }
-        };
-        if (!logo.src.includes(newLogoSrc)) {
-          logo.style.transform = `translateX(${targetTranslation}px)`;
-        }
-
-        showNew();
-      } else if (scrollPosition <= 100 && !logo.src.includes(originalLogoSrc)) {
-        logo.style.opacity = 0;
-
-        logo.style.transform = `translateX(${0}px)`;
-        logo.src = originalLogoSrc;
-        logo.classList.remove('logo--scrolled');
-
-        setTimeout(() => {
-          logo.style.transition = 'opacity 0.5s fade-in';
-          logo.style.opacity = 1;
-        }, 100);
+  main.addEventListener('scroll', function () {
+    if (window.innerWidth <= 768) {
+      if (main.scrollTop > 100) {
+        body.classList.add('scrolled');
+      } else {
+        body.classList.remove('scrolled');
       }
     }
-  }
-
-  document
-    .getElementsByTagName('main')[0]
-    .addEventListener('scroll', handleScroll);
+  });
 });
